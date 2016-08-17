463fcd633e93e3116019628abcde438e4ddd949e
Boris <mbaforever@gmail.com>
2016-08-17 12:12:07 +0300

    first commit

---
+++ doors_backend/models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from _json import make_encoder
+
+from django.db import models
+from utils import thumnails
+import os
---
+++ doors_backend/serializers.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+
+
+
+
